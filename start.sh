1a05e088b2feae83afc3f5eba419f2990c27c603
matrizz <henriqueluizsoarestop10@gmail.com>
2025-07-24 02:29:05 -0300

    update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 
 echo "🚀 Iniciando o bot com PM2..."
 
+mkdir db
+
+echo "Criando pastas..."
+
+touch db/database.json
+
+echo "Criando arquivo de banco de dados..."
+
 pm2 start index.js --name matrizz.java || { echo "❌ Falha ao iniciar o bot."; exit 1; }
 
 pm2 save
@@ -9,3 +17,5 @@ pm2 save
 pm2 startup | tail -n 1 | bash
 
 echo "✅ Bot iniciado com sucesso e configurado para iniciar no boot!"
+
+echo "🚀 Para encerrar o bot, use o comando 'pm2 stop matrizz.java'."
