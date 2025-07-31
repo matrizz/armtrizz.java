2a97e53e62a3615eaf5acba85e633f6a412d6990
matrizz <henriqueluizsoarestop10@gmail.com>
2025-07-31 13:32:04 -0300

    feat: add /anitrack command to monitor anime releases

--- events/botOnline.js
+++ events/botOnline.js
@@ -1,6 +1,8 @@
 require('../index')
+const { checkAnimes } = require('../anitrack')
 const { client } = require('../index')
 
-client.on('ready', async () => {
+client.once('ready', async () => {
     console.log(`🔥 Estou online em ${client.user.username}!`)
+    setInterval(checkAnimes, 60 * 1000)
 })
